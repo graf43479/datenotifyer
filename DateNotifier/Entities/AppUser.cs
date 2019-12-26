b141a471bbf81e6431d3a3631e032cb0236f7ad6
Воронцов Олег Валерьевич <vorontsov@root.ians.aero>
2019-12-26 17:57:46 +0300

    chore(gitignore): gitignore

--- DateNotifier/Entities/AppUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace DateNotifier.Entities
-{
-    public class AppUser : IdentityUser
-    {        
-        public virtual ClientProfile ClientProfile { get; set; }
-    }
-}
